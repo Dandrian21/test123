30796ba662751b2c5d3e5fd7211f224c7d94dc4e
Dandrian21 <94773535+Dandrian21@users.noreply.github.com>
2022-03-09 01:20:41 +0800

    Add files via upload

---
+++ Untitled-1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<div>
+<?php
+
+// PHP program to obtain IP address of
+// the server
+
+// Creating a variable to store the
+// server address
+$localIP = getHostbyName(getHostName());
+  
+// Displaying the address 
+echo $localIP;
+
+?>
+</div>
+</body>
+</html>
